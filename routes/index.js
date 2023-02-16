15ec317454a1776ab9f4d5a59543ad7040c56790
DaYeon Jeaog <glue0440@gmail.com>
2023-02-16 18:22:39 +0900

    styling index page

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,16 @@ router.get('/', function(req, res, next) {
   res.render('index');
 });
 
+router.get('/inputTab', function(req, res, next){
+  res.render('inputTab');
+});
+
+router.get('/logTab', function(req, res, next){
+  res.render('logTab');
+});
+
+router.get('/testTab', function(req, res, next){
+  res.render('testTab');
+});
+
 module.exports = router;
